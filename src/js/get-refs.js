c78a1be4ff00c4de7df76fd364a967e9f6532eaf
Dmytro Petrenko <blinker182ca@gmail.com>
2020-11-16 01:30:03 +0200

    add js-refs

---
+++ src/js/get-refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+    viewContainer: document.querySelector('.view-container'),
+    searchForm: document.querySelector('.js-search-form'),
+    searchFormInput: document.querySelector('.js-form-input'),
+    galleryContainer: document.querySelector('.js-gallery'),
+}
